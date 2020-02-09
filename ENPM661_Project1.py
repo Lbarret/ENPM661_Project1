20163699119ec00f54eb4a34b6e736cec81d33ce
Loic Barret <lbarret94@gmail.com>
2020-02-09 17:54:06 -0500

    Fixed functions so that functions don't alter input node

--- ENPM661_Project1.py
+++ ENPM661_Project1.py
@@ -22,43 +22,50 @@ def FindIndex(CurrentNode):
 
 #This function moves the blank tile to the left or returns 1 if it can't 
 def ActionMoveLeft(CurrentNode):
-    index = FindIndex(CurrentNode)
-    if BlankTileLocation(CurrentNode)[1] != 1:
-        CurrentNode[index] = CurrentNode[index-3]
-        CurrentNode[index-3] = 0
-        return CurrentNode
+    NewNode = CurrentNode[:]
+    index = FindIndex(NewNode)
+    if BlankTileLocation(NewNode)[1] != 1:
+        NewNode[index] = NewNode[index-3]
+        NewNode[index-3] = 0
+        return NewNode
     else:
         return 1
 
 #This function moves the blank tile to the right or returns 1 if it can't 
 def ActionMoveRight(CurrentNode):
-    index = FindIndex(CurrentNode)
-    if BlankTileLocation(CurrentNode)[1] != 3:
-        CurrentNode[index] = CurrentNode[index+3]
-        CurrentNode[index+3] = 0
-        return CurrentNode
+    NewNode = CurrentNode[:]
+    index = FindIndex(NewNode)
+    if BlankTileLocation(NewNode)[1] != 3:
+        NewNode[index] = NewNode[index+3]
+        NewNode[index+3] = 0
+        return NewNode
     else:
         return 1    
     
 #This function moves the blank tile up or returns 1 if it can't 
 def ActionMoveUp(CurrentNode):
-    index = FindIndex(CurrentNode)
-    if BlankTileLocation(CurrentNode)[0] != 1:
-        CurrentNode[index] = CurrentNode[index-1]
-        CurrentNode[index-1] = 0
-        return CurrentNode
+    NewNode = CurrentNode[:]
+    index = FindIndex(NewNode)
+    if BlankTileLocation(NewNode)[0] != 1:
+        NewNode[index] = NewNode[index-1]
+        NewNode[index-1] = 0
+        return NewNode
     else:
         return 1
     
 #This function moves the blank tile down or returns 1 if it can't 
 def ActionMoveDown(CurrentNode):
-    index = FindIndex(CurrentNode)
-    if BlankTileLocation(CurrentNode)[0] != 3:
-        CurrentNode[index] = CurrentNode[index+1]
-        CurrentNode[index+1] = 0
-        return CurrentNode
+    NewNode = CurrentNode[:]
+    index = FindIndex(NewNode)
+    if BlankTileLocation(NewNode)[0] != 3:
+        NewNode[index] = NewNode[index+1]
+        NewNode[index+1] = 0
+        return NewNode
     else:
         return 1 
 
+print(ActionMoveLeft(goal))
+print(ActionMoveRight(goal))
+print(ActionMoveUp(goal))
 print(ActionMoveDown(goal))
 
