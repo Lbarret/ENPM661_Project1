c1f2f3f8473678a49184414417353f5c461037c2
Loic Barret <lbarret94@gmail.com>
2020-02-09 17:43:14 -0500

    Created functions to locate blank tile and move blank tile

--- ENPM661_Project1.py
+++ ENPM661_Project1.py
@@ -16,9 +16,11 @@ def BlankTileLocation(CurrentNode):
         
     return (i,j)
 
+#This function finds the index of the blank tile
 def FindIndex(CurrentNode):
     return (BlankTileLocation(CurrentNode)[1]-1)*3+BlankTileLocation(CurrentNode)[0]-1
 
+#This function moves the blank tile to the left or returns 1 if it can't 
 def ActionMoveLeft(CurrentNode):
     index = FindIndex(CurrentNode)
     if BlankTileLocation(CurrentNode)[1] != 1:
@@ -28,6 +30,7 @@ def ActionMoveLeft(CurrentNode):
     else:
         return 1
 
+#This function moves the blank tile to the right or returns 1 if it can't 
 def ActionMoveRight(CurrentNode):
     index = FindIndex(CurrentNode)
     if BlankTileLocation(CurrentNode)[1] != 3:
@@ -37,6 +40,7 @@ def ActionMoveRight(CurrentNode):
     else:
         return 1    
     
+#This function moves the blank tile up or returns 1 if it can't 
 def ActionMoveUp(CurrentNode):
     index = FindIndex(CurrentNode)
     if BlankTileLocation(CurrentNode)[0] != 1:
@@ -44,8 +48,9 @@ def ActionMoveUp(CurrentNode):
         CurrentNode[index-1] = 0
         return CurrentNode
     else:
-        return 1 
-
+        return 1
+    
+#This function moves the blank tile down or returns 1 if it can't 
 def ActionMoveDown(CurrentNode):
     index = FindIndex(CurrentNode)
     if BlankTileLocation(CurrentNode)[0] != 3:
