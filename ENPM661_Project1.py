a59c3d9935b8381ddab3f8cb7eb37e074d4b0722
Loic Barret <lbarret94@gmail.com>
2020-02-15 16:11:17 -0500

    Fixed solvability check

--- ENPM661_Project1.py
+++ ENPM661_Project1.py
@@ -1,7 +1,7 @@
 
-#This is the goal node
-GoalNode = [0, 3, 6, 1, 4, 7, 2, 5, 8]
-StartNode = [1, 3, 7, 8, 5, 6, 2, 0, 4]
+#This is the Start node
+StartNode = [8, 4, 7, 1, 6, 5, 3, 2, 0]
+GoalNode =  [1, 4, 7, 2, 5, 8, 3, 6, 0]
 Pathlist = []
 Node_list = [StartNode]
 ParentNode_dic = {tuple(StartNode):0}
@@ -9,6 +9,8 @@ inv = 0
 iterator = 0
 
 
+checkstart = [StartNode[0], StartNode[3], StartNode[6], StartNode[1], StartNode[4], StartNode[7], StartNode[2], StartNode[5], StartNode[8]]
+checkstart.pop(checkstart.index(0))
 
 def BlankTileLocation(CurrentNode):
 
@@ -72,9 +74,9 @@ def AddNode(NewNode,Parent):
             Node_list.append(NewNode) 
             ParentNode_dic[tuple(NewNode)]= tuple(Parent)
 
-for i in StartNode:
-    for j in range(i+1,len(StartNode)):
-        if StartNode[i]>StartNode[j]:
+for i in range(7):
+    for j in range(i+1,8):
+        if checkstart[i] > checkstart[j]:
             inv += 1
 
 while inv%2 == 0:
@@ -92,22 +94,16 @@ while inv%2 == 0:
     AddNode(ActionMoveRight(CurrentNode),CurrentNode)
     AddNode(ActionMoveDown(CurrentNode),CurrentNode)
     iterator+=1
+        
     
-    
-    
-    
-    
-if inv%2 == 0:
-    for listitem in Pathlist:
-       print(listitem)
-else:
+if inv%2 != 0:
     print("Puzzle is unsolvable")
+    
 
 with open('NodesInfo.txt', "w") as Nodeinfo:
 
     for nodes in range(1,len(Pathlist)):
         Nodeinfo.write(str(Node_list.index(list(Pathlist[nodes]))) + ' ' + str(Node_list.index(list(Pathlist[nodes-1]))) + '\n')
-        print(str(Node_list.index(list(Pathlist[nodes]))) + ' ' + str(Node_list.index(list(Pathlist[nodes-1]))))
 Nodeinfo.close()       
 
 with open('nodePath.txt', "w") as PathFile:
